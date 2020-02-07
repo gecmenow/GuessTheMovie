eff73eb6cc01ef46a06735ce5e1b2607e5bee725
gecmenow <gector456@gmail.com>
2020-02-07 23:29:01 +0200

    added admin vm

---
+++ GuessTheMovie/Models/ViewModels/AdminVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GuessTheMovie.Models.ViewModels
+{
+    public class AdminVM
+    {
+        public int AdminCode { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
